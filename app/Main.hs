1ef85f2d2c6d0323f2b8964413c7532f716980ee
Ludvig Sundström <lud.sund@gmail.com>
2019-06-09 19:53:55 +0200

    Add new path from command line

--- app/Main.hs
+++ app/Main.hs
@@ -4,8 +4,11 @@ import qualified Data.List as L
 import Data.List.Split (splitOn)
 import Data.Map (Map)
 import qualified Data.Map as M
+import System.Directory
 import System.Environment
 import System.Process
+
+dirSpecFile = "/home/lsund/Documents/tech/repos/piggy/resources/dirs.csv"
 
 tuplify2 :: [a] -> (a, a)
 tuplify2 [x, y] = (x, y)
@@ -17,17 +20,30 @@ match = L.isInfixOf
 fmtDirs :: Map String String -> String
 fmtDirs = concat . L.intersperse "\n" . M.elems
 
-handleCommand :: Map String String -> [String] -> String
-handleCommand dirmap ("cd":x:_) = fmtDirs $ M.filterWithKey (const . (match x)) dirmap
-handleCommand dirmap ("cdl":_) = fmtDirs $ dirmap
+addDirTo :: FilePath -> String -> String -> IO String
+addDirTo fname tag path = do
+  cwd <- getCurrentDirectory
+  return "OK" <$>
+    (appendFile fname $
+     "\n" <> tag <> "," <>
+     if path == "."
+       then cwd
+       else path)
+
+handleCommand :: Map String String -> [String] -> IO String
+handleCommand dirmap ("cd":x:_) =
+  return . fmtDirs $ M.filterWithKey (const . (match x)) dirmap
+handleCommand dirmap ("cdl":_) = return . fmtDirs $ dirmap
+handleCommand dirmap ("a":tag:path:_) = addDirTo dirSpecFile tag path
 handleCommand _ _ = undefined
 
-readDirsFrom :: String -> IO (Map String String)
-readDirsFrom path =
+readDirsFrom :: FilePath -> IO (Map String String)
+readDirsFrom fname =
   M.fromList <$>
-  ((mapM (return . tuplify2 . splitOn ",") . lines) =<< readFile path)
+  ((mapM (return . tuplify2 . splitOn ",") . lines) =<< readFile fname)
 
 main :: IO ()
 main = do
-  dirs <- readDirsFrom "resources/dirs.csv"
-  (putStrLn . handleCommand dirs) =<< getArgs
+  dirs <- readDirsFrom dirSpecFile
+  s <- (handleCommand dirs) =<< getArgs
+  putStrLn s
