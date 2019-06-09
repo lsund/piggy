4216e7fe792061b97ea8eca953cc343149e55723
Ludvig Sundström <lud.sund@gmail.com>
2019-06-09 20:10:04 +0200

    Linting

--- app/Main.hs
+++ app/Main.hs
@@ -4,10 +4,11 @@ import qualified Data.List as L
 import Data.List.Split (splitOn)
 import Data.Map (Map)
 import qualified Data.Map as M
+import Prelude
 import System.Directory
 import System.Environment
-import System.Process
 
+dirSpecFile :: FilePath
 dirSpecFile = "/home/lsund/Documents/tech/repos/piggy/resources/dirs.csv"
 
 tuplify2 :: [a] -> (a, a)
@@ -18,23 +19,24 @@ match :: String -> String -> Bool
 match = L.isInfixOf
 
 fmtDirs :: Map String String -> String
-fmtDirs = concat . L.intersperse "\n" . M.elems
+fmtDirs = L.intercalate "\n" . M.elems
 
 addDirTo :: FilePath -> String -> String -> IO String
 addDirTo fname tag path = do
   cwd <- getCurrentDirectory
-  return "OK" <$>
-    (appendFile fname $
-     "\n" <> tag <> "," <>
-     if path == "."
-       then cwd
-       else path)
+  "OK" <$
+    appendFile
+      fname
+      ("\n" <> tag <> "," <>
+       if path == "."
+         then cwd
+         else path)
 
 handleCommand :: Map String String -> [String] -> IO String
 handleCommand dirmap ("cd":x:_) =
-  return . fmtDirs $ M.filterWithKey (const . (match x)) dirmap
+  return . fmtDirs $ M.filterWithKey (const . match x) dirmap
 handleCommand dirmap ("cdl":_) = return . fmtDirs $ dirmap
-handleCommand dirmap ("a":tag:path:_) = addDirTo dirSpecFile tag path
+handleCommand _ ("a":tag:path:_) = addDirTo dirSpecFile tag path
 handleCommand _ _ = undefined
 
 readDirsFrom :: FilePath -> IO (Map String String)
@@ -45,5 +47,5 @@ readDirsFrom fname =
 main :: IO ()
 main = do
   dirs <- readDirsFrom dirSpecFile
-  s <- (handleCommand dirs) =<< getArgs
+  s <- handleCommand dirs =<< getArgs
   putStrLn s
