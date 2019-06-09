f2fda555b81bd179a8d5e9460cb7987c76d008be
Ludvig Sundström <lud.sund@gmail.com>
2019-06-09 19:22:08 +0200

    Read dirs from file

--- app/Main.hs
+++ app/Main.hs
@@ -1,21 +1,15 @@
 module Main where
 
 import qualified Data.List as L
+import Data.List.Split (splitOn)
 import Data.Map (Map)
 import qualified Data.Map as M
 import System.Environment
 import System.Process
 
-dirmap :: Map String String
-dirmap =
-  M.fromList
-    [ ("scripts", "/home/lsund/Documents/scripts")
-    , ("dotfiles", "/home/lsund/Documents/dotfiles")
-    , ("finances", "/home/lsund/Documents/tech/repos/finances")
-    , ("trainer", "/home/lsund/Documents/tech/repos/trainer")
-    , ("repos", "/home/lsund/Documents/tech/repos")
-    , ("goals", "/home/lsund/Documents/tech/repos/goals")
-    ]
+tuplify2 :: [a] -> (a, a)
+tuplify2 [x, y] = (x, y)
+tuplify2 _ = undefined
 
 match :: String -> String -> Bool
 match = L.isInfixOf
@@ -23,10 +17,17 @@ match = L.isInfixOf
 fmtDirs :: Map String String -> String
 fmtDirs = concat . L.intersperse "\n" . M.elems
 
-handleCommand :: [String] -> String
-handleCommand ("cd":x:_) = fmtDirs $ M.filterWithKey (const . (match x)) dirmap
-handleCommand ("cdl":_) = fmtDirs $ dirmap
-handleCommand _ = undefined
+handleCommand :: Map String String -> [String] -> String
+handleCommand dirmap ("cd":x:_) = fmtDirs $ M.filterWithKey (const . (match x)) dirmap
+handleCommand dirmap ("cdl":_) = fmtDirs $ dirmap
+handleCommand _ _ = undefined
+
+readDirsFrom :: String -> IO (Map String String)
+readDirsFrom path =
+  M.fromList <$>
+  ((mapM (return . tuplify2 . splitOn ",") . lines) =<< readFile path)
 
 main :: IO ()
-main = (putStrLn . handleCommand) =<< getArgs
+main = do
+  dirs <- readDirsFrom "resources/dirs.csv"
+  (putStrLn . handleCommand dirs) =<< getArgs
